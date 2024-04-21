1bbaab407fd6353541ebc85e685248b45a8e9b1f
chemicoding <jose.miguelgonzalezdominguez@gmail.com>
2024-04-21 21:03:25 +0200

    Changed template for Astro Blog and added animated mode 7 style logo

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+	site_name: 'Power UP!',
+	title: 'Power UP! | Shmups and more',
+	description: 'Boilerplate built with Astro and Tailwind CSS',
+	author: 'Nicolas',
+	locale_region: 'de-en',
+	locale: 'en'
+};
